e573de862408fccfa820097a2b41fdb201241d16
Syracusa <msrndsy@gmail.com>
2018-01-08 16:48:46 +0900

    add flame.ino

---
+++ flame.ino
@@ -0,0 +1,15 @@
+#define flame A3
+
+int fm;
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ pinMode(flame,INPUT); // flame Pin is A3
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+fm = analogRead(flame); // read flame
+Serial.println(fm);
+delay(200) // delay 200ms
+}
