0a733a874e565de5faf5b4bea8be8dfe38deac37
Syracusa <msrndsy@gmail.com>
2018-01-16 16:38:18 +0900

    add siren.ino

---
+++ siren.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+for(int freq = 150;freq<=1800;freq=freq+2){
+  tone(9,freq,10);
+  }
+
+  for(int freq = 1800; freq <=150; freq = freq-2){
+    tone(9,freq,10);
+    }
+
+}
